07604847505d51f31c6e2f7c090d0c1a4c079939
ProgmanGame <100918882+ProgmanGame@users.noreply.github.com>
2023-04-04 21:26:46 +0300

    Add files via upload

---
+++ eninem_godzila.js
@@ -0,0 +1,21 @@
+let playlist = {}
+playlist.text =`If you never gave a damn, raise your hand 'Cause I'm about to set trip, vacation plans I'm on point like my index is, So all you will ever get is the motherfuckin' finger (Finger), Prostate exam ('Xam) How can I have all these fans and perspire? Like a liar's pants, I'm on fire And I got no plans to retire And I'm still the man you admire These chicks are spazzin' out, I only get more handsome and flyer I got 'em passin' out like what you do When you hand someone flyers
+And what goes around comes around Just like the blades on the chainsaw 'Cause I caught the flack, but my dollars stacked right off the bat Like a baseball Like Kid Ink, bitch, I got them racks with so much ease that they call me Diddy 'Cause I make bands And I call getting cheese a cakewalk
+Bitch, I'm a player, I'm too motherfuckin' stingy for Cher Won't even lend you an ear, Ain't even pretending to care But I tell a bitch I'll marry her If she'll bury her Face in my genital area The original Richard Ramirez, Cristhian Rivera 'Cause my lyrics never sit well So they wanna give me the chair Like a paraplegic, and it's scary, Call it Hari Kari 'Cause e'ry Tom and Dick and Harry Carry a Merriam motherfuckin' dictionary on 'em Swearing up and down they can spit, This shit's hilarious It's time to put these bitches in the obituary column We wouldn't see eye to eye With a staring problem Get the shaft like a steering columnp 
+Trigger happy, pack heat, But it's black ink Evil half of The Bad Meets Evil, That means take a back seat Take it back to Fat Beats with a maxi single Look at my rap sheet, What attracts these people Is my 'Gangsta Bitch' like Apache With a catchy jingle I stack chips, You barely got a half-eaten Cheeto
+Fill 'em with the venom and eliminate 'em Other words, I Minute Maid 'em I don't wanna hurt 'em, but I did, I'm in a fit of rage I'm murderin' again, nobody will evade I'm finna kill 'em And dump all their fuckin' bodies in the lake Obliterating everything, incinerate a renegade I'm here to make anybody who want it With the pen afraid But don't nobody want it, But they're gonna get it anyway 'Cause I'm beginnin' to feel like I'm mentally ill I'm Attila, kill or be killed, I'm a killer bee, the vanilla gorilla You're bringin' the killer within me outta me You don't wanna be the enemy of the demon Who entered me And be on the receivin' end of me, What stupidity it'd be Every bit of me's the epitome of a spitter When I'm in the vicinity, motherfucker, you better duck Or you finna be dead the minute You run into me A hundred percent of you is a fifth of a percent of me I'm 'bout to fuckin' finish you, bitch, I'm unfadable You wanna battle, I'm available, I'm blowin' up like an inflatable I'm undebatable, I'm unavoidable, I'm unevadable I'm on the toilet bowl, I got a trailer full of money And I'm paid in full I'm not afraid to pull
+a Man, stop Look what I'm plannin', haha`
+playlist.lang="eng"
+playlist.urlSound = "./music/Eminem_Juice_WRLD_-_Godzilla.mp3";
+playlist.stop=[
+    {music:11.9, text: 200 },       // 16.8     59.52
+    {music: 23.68 , text: 474 },    // 23.26    43
+    {music: 36, text: 766 },        // 23.7     42.19
+    {music: 72, text: 1715 },       // 26.38    37.9
+    {music: 101.9, text: 2860 },    // 38.3     26.1
+    {music: 105, text: 2900 },      // 13       76.9
+];
+playlist.pause = 50
+playlist.volume = 0.7
+playlist.start = 0
+playlist.end = 105     // длина секунд до последей буквы текста
